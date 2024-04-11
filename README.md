89d478e03fb497884740a4cc563314f0a331bcb5
Caleb Smitheart <smitheartc@gmail.com>
2024-04-10 23:51:54 -0500

    I got excited and followed a tutorial

--- README.md
+++
@@ -1,1 +0,0 @@
-"# AKPsiCSCase2" 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/lyrics/The Weeknd/Starboy').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.status === 200 ? 
+          <p>{getMessage.data.lyrics}</p>
+          :
+          <p>LOADING</p>}</div>
       </header>
     </div>
   );
 }
-
 export default App;
