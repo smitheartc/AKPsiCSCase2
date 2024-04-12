19f127c6bcfd7b4080d2763e756fad606f6dd7ff
Veer Waje <wajeveer@gmail.com>
2024-04-12 02:18:03 -0500

    set up logic for theme generation

--- api/themeApi.py
+++ api/themeApi.py
@@ -3,32 +3,38 @@ from sklearn.feature_extraction.text import CountVectorizer
 import requests
 import json
 
-
 class themeApi(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('lyrics', type=str)
+        args = parser.parse_args()
 
-  def post(self):
-    parser = reqparse.RequestParser()
-    parser.add_argument('lyrics', type=str)
-    args = parser.parse_args()
+        # note, the post req from frontend needs to match the string here
+        lyrics = args['lyrics']
 
-    # note, the post req from frontend needs to match the string here
-    lyrics = args['lyrics']
+        # changing it to a string format
+        lyrics = json.dumps(lyrics)
 
-    # changing it to a string format
-    lyrics = json.dumps(lyrics)
+        vectorizer = CountVectorizer()
+        X = vectorizer.fit_transform(lyrics)
 
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(lyrics)
-    z = vectorizer.vocabulary
-        
-    #Cleaning up the junk in the first line
-    # raw = resp.json()
-    # lyrics = raw["lyrics"]
-    # index = lyrics.find('\n')  #finding the first \n
-    # lyrics = lyrics[index+1:]  #deleting everything before that
+        feature_names = vectorizer.get_feature_names_out()
 
-    # #Making the return a json
-    # lyricList = {"lyrics" : lyrics}
-    # response = json.dumps(lyricList)
+        df = pd.DataFrame(X.toarray(), columns=feature_names)
 
-    return z
+        sentiment_scores = np.zeros(df.shape[1])
+        for i in range(df.shape[1]):
+            word = feature_names[i]
+            if word in positive_words:
+                sentiment_scores[i] += 1
+            elif word in negative_words:
+                sentiment_scores[i] -= 1
+
+        # Get the top 2 words with the highest sentiment scores
+        top_2_words = np.argsort(sentiment_scores)[-2:]
+
+        #Making the return a json
+        jsonRet = {"theme" : feature_names[top_2_words[0]]}
+        response = json.dumps(jsonRet)
+
+        return response
