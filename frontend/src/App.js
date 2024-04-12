d7185d27561b7ff71ad7bfbac102656276bbe0f3
Sanyukta26 <86387930+Sanyukta26@users.noreply.github.com>
2024-04-11 23:23:29 -0500

    background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   }, [])
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>React + Flask Tutorial</p>
