0e8777fce104cd41966a154aa166ac657e1499e6
Caleb Smitheart <smitheartc@gmail.com>
2024-04-11 20:52:29 -0500

    New API Development

---
+++ api/translateApi.py
@@ -0,0 +1,32 @@
+from flask_restful import Api, Resource, reqparse
+import requests
+import json
+
+class translateApi(Resource):
+
+  def post(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument('artist', type=str)
+    parser.add_argument('song', type=str)
+    args = parser.parse_args()
+
+    # note, the post req from frontend needs to match the strings here (e.g. 'artist and 'song')
+    artist = args['artist']
+    song = args['song']
+
+    #formatting the request arguments properly by removing spaces
+    artist.replace("%20"," ")
+    song.replace("%20"," ")
+    resp = requests.get("https://api.lyrics.ovh/v1/{artist}/{song}".format(artist=artist, song=song))
+        
+    #Cleaning up the junk in the first line
+    raw = resp.json()
+    lyrics = raw["lyrics"]
+    index = lyrics.find('\n')  #finding the first \n
+    lyrics = lyrics[index+1:]  #deleting everything before that
+
+    #Making the return a json
+    lyricList = {"lyrics" : lyrics}
+    response = json.dumps(lyricList)
+
+    return response
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
+from api.lyricApi import lyricApi
+from api.translateApi import translateApi
+from api.themeApi import themeApi
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
@@ -11,4 +13,6 @@ api = Api(app)
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
-api.add_resource(HelloApiHandler, '/api/')
+api.add_resource(lyricApi, '/lyrics/')
+api.add_resource(translateApi, '/translate/')
+api.add_resource(themeApi, '/theme/')
