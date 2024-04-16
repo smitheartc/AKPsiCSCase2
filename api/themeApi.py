d80aaaaa49a8025365b33fd08893534c27f96d46
Veer Waje <wajeveer@gmail.com>
2024-04-15 23:35:12 -0500

    fixing logic and improving time complexity

--- api/themeApi.py
+++ api/themeApi.py
@@ -15,7 +15,7 @@ class themeApi(Resource):
         # changing it to a string format
         lyrics = json.dumps(lyrics)
 
-        #initialize countvectorizer, configuring it to consider key words
+        #initialize countvectorizer, removing irrelevant words and also considering 1 and 2 word phrases
         vectorizer = CountVectorizer(stop_words='english', ngram_range=(1, 2))
         
         #giving the CV the lyrics
@@ -23,15 +23,15 @@ class themeApi(Resource):
 
         # Sum the occurrences of each word
         word_counts = X.sum(axis=0)  
-        words_freq = [(word, word_counts[0, idx]) for word, idx in vectorizer.vocabulary_.items()]
-        #reverse sorts it
-        words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
 
-        #remove short words
-        new_words_freq = [word for word, count in words_freq if len(word) > 3][:2]
-
-        #getting the top 2 words freq words
-        returnDict = new_words_freq[:2]
+        x = None
+        mc = -1
+        # iterate over all the words in the vocab items and get most frequently occuring word
+        for word, idx in vectorizer.vocabulary_.items():
+            curr = word_counts[0, idx]
+            if curr > mc:
+                x = word
+                mc = curr
         
         #Making the return a json
         return jsonify(returnDict)
