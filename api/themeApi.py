6763b3c1a8dbfb7b751c40f8e5f7eb8877014dc0
Veer Waje <wajeveer@gmail.com>
2024-04-15 22:39:39 -0500

    removing very short words

--- api/themeApi.py
+++ api/themeApi.py
@@ -24,10 +24,14 @@ class themeApi(Resource):
         # Sum the occurrences of each word
         word_counts = X.sum(axis=0)  
         words_freq = [(word, word_counts[0, idx]) for word, idx in vectorizer.vocabulary_.items()]
+        #reverse sorts it
         words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
 
+        #remove short words
+        new_words_freq = [word for word, count in words_freq if len(word) > 3][:2]
+
         #getting the top 2 words freq words
-        returnDict = words_freq[:2]
+        returnDict = new_words_freq[:2]
         
         #Making the return a json
         return jsonify(returnDict)
