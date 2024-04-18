d2b50920d130cee081a037fed18f40ab3c66c54c
Veer Waje <152214992+vwaje13@users.noreply.github.com>
2024-04-18 15:32:59 -0500

    changing stop words for theme

--- api/themeApi.py
+++ api/themeApi.py
@@ -20,7 +20,7 @@ class themeApi(Resource):
 
         #change stop words (what words are important vs not)
         sw = set(ENGLISH_STOP_WORDS)
-        sw.update(['ha', 'oh', 'll', 'yeah', 'like'])
+        sw.update(['ha', 'oh', 'll', 'yeah', 'like', 'uh', 'nah'])
         list_sw = list(sw)
 
         #initialize countvectorizer, removing stop words and looking for 1-2 word length phrases
@@ -29,25 +29,18 @@ class themeApi(Resource):
         #giving the CV the lyrics
         cv = vectorizer.fit_transform([lyrics])
 
+        #using LDA to get a word that most represents the topic
         lda = LatentDirichletAllocation(n_components=1, random_state=0)  # One topic
+
+        #fitting it to the document term matrix
         lda.fit(cv)
 
+        #getting the names within the document term matrix
         names = vectorizer.get_feature_names_out()
 
+        #iterates throught the lda
         for topic_idx, topic in enumerate(lda.components_):
             message = " ".join([names[i] for i in topic.argsort()[:-2:-1]])
-
-        # Sum the occurrences of each word
-        # word_counts = cv.sum(axis=0)  
-
-        # x = None
-        # mc = -1
-        # # iterate over all the words in the vocab items and get most frequently occuring word
-        # for word, idx in vectorizer.vocabulary_.items():
-        #     curr = word_counts[0, idx]
-        #     if curr > mc:
-        #         x = word
-        #         mc = curr
         
         #Making the return a json
         theme = {"theme" : message}
