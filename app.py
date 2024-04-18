1de2698d1fc7134b51139b1b4d739f79532af33f
smitheartc <rcs220002@utdallas.edu>
2024-04-18 12:57:57 -0500

    Moving into production

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS #comment this on deployment
+#from flask_cors import CORS #comment this on deployment
 from api.lyricApi import lyricApi
 from api.translateApi import translateApi
 from api.themeApi import themeApi
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-CORS(app) #comment this on deployment
+#CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
